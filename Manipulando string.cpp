abf10e6674bd04d6d4174ddce266543eada23490
Paulo César <paulocesarcs.dev@gmail.com>
2017-02-04 01:30:51 -0200

    Começando estudar hard para maratona

---
+++ Manipulando string.cpp
@@ -0,0 +1,8 @@
+Manipulando string
+Problema Um-Dois-Três
+ https://www.urionlinejudge.com.br/judge/pt/problems/view/1332
+ 
+
+  A receita será gerada através do aluguel da vaga. E reserva de vaga, vamos receber uma (%) do valor da vaga. Atráves do paypal.
+
+  taimanin asagi 2
