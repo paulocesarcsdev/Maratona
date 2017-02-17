a2e18099565604d8fe4d26b62d88e5906f9e2375
Paulo César <paulocesarcs.dev@gmail.com>
2017-02-17 21:12:26 -0200

    Adicionado três problemas

---
+++ Roteiro 0/pneu.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+    int main(){
+	int N, M, TT;
+	scanf("%d%d", &N, &M);
+	TT = (N - M);
+	printf("%d", TT);
+	
+	
+	return 0;
+	}
---
+++ Roteiro 0/tomada.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+    int main (){
+	int T1, T2, T3, T4,TT;
+	
+	scanf("%d%d%d%d", &T1,&T2,&T3,&T4);
+	
+	TT = (T1+T2+T3+T4 - 3);
+	
+	printf("%d", TT);
+	
+	return 0;
+	}
