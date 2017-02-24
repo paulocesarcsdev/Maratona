e581626dc5352f73f5e3afe91fe418b2b05a3721
Paulo César <paulocesarcs.dev@gmail.com>
2017-02-23 21:22:13 -0300

    dobra

---
+++ Roteiro 0/dobra.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+	int main () {
+	int n;	//[16] = {4,9,25.};
+	scanf("%d", &n);
+	n=pow(n,4);
+	scanf("%d", &n);
+	printf("%d", n);
+	//scanf("%d", &n[0]);
+	//printf("%d", n[0]);
+
+}		
